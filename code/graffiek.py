d6cf4b9df8618cf454e899133a3fe44e958bb0bb
BartTho <61376948+BartTho@users.noreply.github.com>
2024-11-14 10:39:39 +0100

    Create graffiek.py

---
+++ code/graffiek.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+Aatal = 2
+
+x = np.arange(0, Aatal*np.pi, 0.1)
+y = np.sin(x)
+plt.plot(x, y, color='green')
+plt.xlabel('tijd(s)')
+plt.ylabel('Waarde')
+plt.savefig('sinus.png')
+plt.show()
